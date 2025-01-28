578a468c65ad1f09995c5af546b3d09017635701
danila nikitin <danila.n2015@yandex.ru>
2025-01-28 17:28:45 +0300

    add dependencies

---
+++ src/main/java/com/jfund/currencyaggregationsservice/CurrencyAggregationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jfund.currencyaggregationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAggregationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyAggregationServiceApplication.class, args);
+    }
+
+}
